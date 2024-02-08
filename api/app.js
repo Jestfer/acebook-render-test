a439fa7c33feb505bbed94195c313b6092b28b48
Jestfer <jestfer6@gmail.com>
2024-02-08 13:06:53 +0000

    modified app.js file

--- api/app.js
+++ api/app.js
@@ -6,53 +6,20 @@ const usersRouter = require("./routes/users");
 const postsRouter = require("./routes/posts");
 const authenticationRouter = require("./routes/authentication");
 const commentsRouter = require("./routes/comments");
-// const tokenChecker = require("./middleware/tokenChecker");
 
 const app = express();
 
-// Allow requests from any client
-// docs: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-// docs: https://expressjs.com/en/resources/middleware/cors.html
 app.use(cors());
-
-//render/deployment handling
-if (process.env.NODE_ENV === "production") {
-  //*Set static folder up in production
-  app.use(express.static("../frontend/dist"));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
-  );
-}
-
-// Parse JSON request bodies, made available on `req.body`
 app.use(bodyParser.json());
 
 // API Routes
-app.use("api/", commentsRouter);
-app.use("/users", usersRouter);
-app.use("/", postsRouter);
-app.use("/tokens", authenticationRouter);
-app.use("/like/:id", postsRouter);
+app.use("/api/comments", commentsRouter); // Fixed the route prefix here
+app.use("/api/users", usersRouter);
+app.use("/api/posts", postsRouter);
+app.use("/api/tokens", authenticationRouter);
 
-// 404 Handler
-app.use((_req, res) => {
-  res.status(404).json({ err: "Error 404: Not Found" });
-});
-
-// Error handler
-app.use((err, _req, res, _next) => {
-  console.error(err);
-
-  if (process.env.NODE_ENV === "development") {
-    res.status(500).send(err.message);
-  } else {
-    res.status(500).json({ err: "Something went wrong" });
-  }
-});
-
+// Static files and SPA fallback in production
 if (process.env.NODE_ENV === "production") {
-  //*Set static folder up in production
   console.log("Setting up static files from:", path.resolve(__dirname, '../frontend/dist'));
   app.use(express.static(path.resolve(__dirname, '../frontend/dist')));
 
@@ -62,5 +29,16 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(indexPath);
   });
 }
+
+// 404 Handler should be after API and static routes
+app.use((_req, res) => {
+  res.status(404).json({ err: "Error 404: Not Found" });
+});
+
+// General error handler
+app.use((err, _req, res, _next) => {
+  console.error(err);
+  res.status(500).json({ err: "Something went wrong" });
+});
 
 module.exports = app;
